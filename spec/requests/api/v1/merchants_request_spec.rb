abead56b75790f25d0ea68e7b08a7ff431e2c362
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-07 17:46:53 -0700

    add test for getting 1 merchant by ID and route/action for show in merchants controller

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -21,4 +21,20 @@ describe "Merchants API" do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(merchant[:attributes]).to have_key(:id)
+    expect(merchant[:attributes][:id]).to be_an(Integer)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
+  end
 end
