186add110d0ad654d22ea84a007f76a00ecfa4d1
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-10 14:08:33 -0700

    remove obsolete nil conditionals from index action

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -2,32 +2,16 @@ class Api::V1::ItemsSearchController < ApplicationController
   def index
     if params[:name]
       items = Item.find_all_items_by_name(params[:name])
-      if items.nil?
-        render json: {data: {message: "No items match your search"}}
-      else
-        render json: ItemSerializer.new(items)
-      end
+      render json: ItemSerializer.new(items)
     elsif params[:min_price] && params[:max_price]
       items = Item.find_all_items_by_price_range(params[:min_price], params[:max_price])
-      if items.nil?
-        render json: {data: {message: "No items match your search"}}
-      else
-        render json: ItemSerializer.new(items)
-      end
+      render json: ItemSerializer.new(items)
     elsif params[:min_price]
       items = Item.find_all_items_by_min_price(params[:min_price])
-      if items.nil?
-        render json: {data: {message: "No items match your search"}}
-      else
-        render json: ItemSerializer.new(items)
-      end
+      render json: ItemSerializer.new(items)
     elsif params[:max_price]
       items = Item.find_all_items_by_max_price(params[:max_price])
-      if items.nil?
-        render json: {data: {message: "No items match your search"}}
-      else
-        render json: ItemSerializer.new(items)
-      end
+      render json: ItemSerializer.new(items)
     end
   end
 end
--- spec/requests/api/v1/items_search_spec.rb
+++ spec/requests/api/v1/items_search_spec.rb
@@ -143,7 +143,7 @@ describe 'All Items Search API endpoint' do
     end
   end
 
-  it 'returns nil if an item does not exist' do
+  it 'returns nil if an item does not exist by searched name' do
     merchant_1 = create(:merchant, name: "ABC")
     merchant_2 = create(:merchant, name: "DEF")
 
@@ -156,7 +156,32 @@ describe 'All Items Search API endpoint' do
     expect(parsed_search).to have_key(:data)
     expect(parsed_search[:data]).to be_an(Array)
     expect(parsed_search[:data].length).to eq(0)
-    # expect(parsed_search[:message]).to eq("No items match your search")
-    #change message and the parsed_search variable in merchant
+  end
+
+  it 'returns nil if an item does not exist by searched price' do
+    merchant_1 = create(:merchant)
+    item_1 = merchant_1.items.create!(name: "Item 1 Name", description: "Item 1 Description", unit_price: 5)
+    item_2 = merchant_1.items.create!(name: "Item 2 Name", description: "Item 2 Description", unit_price: 25)
+    item_3 = merchant_1.items.create!(name: "Item 3 Name", description: "Item 3 Description", unit_price: 30)
+    item_4 = merchant_1.items.create!(name: "Item 4 Name", description: "Item 4 Description", unit_price: 175)
+    merchant_2 = create(:merchant)
+    item_5 = merchant_2.items.create!(name: "Item 5 Name", description: "Item 5 Description", unit_price: 1150)
+    item_6 = merchant_2.items.create!(name: "Item 6 Name", description: "Item 6 Description", unit_price: 40)
+    item_7 = merchant_2.items.create!(name: "Item 7 Name", description: "Item 7 Description", unit_price: 200)
+    merchant_3 = create(:merchant)
+    item_8 = merchant_3.items.create!(name: "Item 8 Name", description: "Item 8 Description", unit_price: 300)
+    item_9 = merchant_3.items.create!(name: "Item 9 Name", description: "Item 9 Description", unit_price: 40)
+    item_10 = merchant_3.items.create!(name: "Item 10 Name", description: "Item 10 Description", unit_price: 10)
+    item_11 = merchant_3.items.create!(name: "Item 11 Name", description: "Item 11 Description", unit_price: 1000)
+
+    search = "max_price=150&min_price=50"
+
+    get "/api/v1/items/find_all?name=#{search}"
+
+    parsed_search = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_search).to have_key(:data)
+    expect(parsed_search[:data]).to be_an(Array)
+    expect(parsed_search[:data].length).to eq(0)
   end
 end
