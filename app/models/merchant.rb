896ed91d2eb6f6601b141823a610d360294a56fa
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-10 08:08:57 -0700

    edit .find_merchant_by_name and test to make search case insensitive but return the first result ordered case sensitive

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
 
   def self.find_merchant_by_name(search)
-    Merchant.where("name ILIKE ?", "%#{search}%" ).limit(1)
+    Merchant.where("name ILIKE ?", "%#{search}%" )
+            .order(:name)
+            .limit(1)
   end
 end
