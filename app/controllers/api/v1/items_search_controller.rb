8eb568079ecb0307034aa4cc4bf1fb1630a8ad71
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-11 13:42:00 -0700

    edit index method to have line render json only at the end of the method

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -1,17 +1,15 @@
 class Api::V1::ItemsSearchController < ApplicationController
   def index
+    # items = if…end
     if params[:name]
       items = Item.find_all_items_by_name(params[:name])
-      render json: ItemSerializer.new(items)
     elsif params[:min_price] && params[:max_price]
       items = Item.find_all_items_by_price_range(params[:min_price], params[:max_price])
-      render json: ItemSerializer.new(items)
     elsif params[:min_price]
       items = Item.find_all_items_by_min_price(params[:min_price])
-      render json: ItemSerializer.new(items)
     elsif params[:max_price]
       items = Item.find_all_items_by_max_price(params[:max_price])
-      render json: ItemSerializer.new(items)
     end
+      render json: ItemSerializer.new(items)
   end
 end
