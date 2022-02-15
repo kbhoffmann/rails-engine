c6b19d9796c5d51214930ee62f5d16e9025693a2
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-15 15:48:32 -0700

    Update merchant.rb
    Romove unneeded Merchant.  from class methods.  Fix indentation.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,26 +7,26 @@ class Merchant < ApplicationRecord
 
 
   def self.find_merchant_by_name(search)
-    Merchant.where("name ILIKE ?", "%#{search}%" )
-            .order(:name)
-            .limit(1)
+    where("name ILIKE ?", "%#{search}%" )
+    .order(:name)
+    .limit(1)
   end
 
   def self.top_merchants_by_revenue(number)
-    Merchant.joins(invoices: [:invoice_items, :transactions])
-            .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
-            .select("SUM(invoice_items.unit_price * invoice_items.quantity) as total_revenue, merchants.*")
-            .group(:id)
-            .order(total_revenue: :desc)
-            .limit(number)
+    joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
+    .select("SUM(invoice_items.unit_price * invoice_items.quantity) as total_revenue, merchants.*")
+    .group(:id)
+    .order(total_revenue: :desc)
+    .limit(number)
   end
 
   def self.top_merchants_by_items_sold(quantity)
-    Merchant.joins(invoices: [:invoice_items, :transactions])
-            .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
-            .select("SUM(invoice_items.quantity) as items_sold, merchants.*")
-            .order(items_sold: :desc)
-            .group(:id)
-            .limit(quantity)
+    joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
+    .select("SUM(invoice_items.quantity) as items_sold, merchants.*")
+    .order(items_sold: :desc)
+    .group(:id)
+    .limit(quantity)
   end
 end
