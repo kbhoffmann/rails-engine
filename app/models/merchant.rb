6daf12a4c79c8b3be241a7d4550f774e2c05dac2
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-15 15:42:08 -0700

    Update merchant.rb
    Remove comment

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,15 +8,12 @@ class Merchant < ApplicationRecord
 
   def self.find_merchant_by_name(search)
     Merchant.where("name ILIKE ?", "%#{search}%" )
-    #can take out Merchant (self)
             .order(:name)
             .limit(1)
   end
 
   def self.top_merchants_by_revenue(number)
     Merchant.joins(invoices: [:invoice_items, :transactions])
-    # GET /api/v1/revenue/merchants?quantity=x
-    #can take out Merchant (self)
             .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
             .select("SUM(invoice_items.unit_price * invoice_items.quantity) as total_revenue, merchants.*")
             .group(:id)
@@ -25,14 +22,11 @@ class Merchant < ApplicationRecord
   end
 
   def self.top_merchants_by_items_sold(quantity)
-    # GET /api/v1/merchants/most_items?quantity=x
-    #can take out Merchant (self)
     Merchant.joins(invoices: [:invoice_items, :transactions])
             .where(transactions: {result: 'success'}, invoices: {status: 'shipped'})
             .select("SUM(invoice_items.quantity) as items_sold, merchants.*")
             .order(items_sold: :desc)
             .group(:id)
             .limit(quantity)
-            # Merchant.joins(invoices: [:invoice_items, :transactions]).where(transactions: {result: 'success'}, invoices: {status: 'shipped'}).select("SUM(invoice_items.quantity) as quantity_sold, merchants.*").order(quantity_sold: :desc).group(:id).limit(5)
   end
 end
